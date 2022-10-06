6f6bc10070adcdaddbaf380eb899f12067a8a712
emosturi <nicsalb@gmail.com>
2022-10-05 23:45:21 -0300

    Features - Added a simple form to add transaccions, unfinished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h2>The Moniac</h2>
+                <input placeholder="Cantidad..." />
+                <br />
+                <label>Desde:</label>
+                <select>
+                    <option>activo</option>
+                    <option>pasivo</option>
+                </select>
+                <br />
+                <label>Hacia:</label>
+                <select>
+                    <option>activo</option>
+                    <option>pasivo</option>
+                </select>
+                <button>Ingresar</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCsjizRRn8bk2pt8QY0RXUXo6BdFb1AjAg",
+    authDomain: "the-moniac.firebaseapp.com",
+    projectId: "the-moniac",
+    storageBucket: "the-moniac.appspot.com",
+    messagingSenderId: "217996707842",
+    appId: "1:217996707842:web:c49a8cb5b7072c92f8bf1b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
